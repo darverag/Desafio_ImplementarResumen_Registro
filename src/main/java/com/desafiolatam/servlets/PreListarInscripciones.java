6ea8cc78788941a5306d1c12dd26a565a685d208
Darío <darverag@gmail.com>
2022-04-02 09:51:10 -0300

    first commit

---
+++ src/main/java/com/desafiolatam/servlets/PreListarInscripciones.java
@@ -0,0 +1,43 @@
+package com.desafiolatam.servlets;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.desafiolatam.entidades.InscripcionDTO;
+import com.desafiolatam.facade.Facade;
+
+@WebServlet("/PreListarInscripciones")
+public class PreListarInscripciones extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<InscripcionDTO>inscripciones;
+		Facade facade =new Facade();
+		try {
+			inscripciones = facade.obtieneInscripciones();
+			request.setAttribute("inscripciones", inscripciones);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		request.getRequestDispatcher("ListaInscripciones.jsp").forward(request,response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
